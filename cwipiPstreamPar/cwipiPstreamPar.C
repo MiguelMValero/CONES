c82cbaec55c8e52be9b534cc4df158e60aea7d72
Lucas <lucas.villanueva@ensma.fr>
2023-08-16 15:39:10 +0200

    cavity test working in parallel

--- cwipiPstreamPar/cwipiPstreamPar.C
+++ cwipiPstreamPar/cwipiPstreamPar.C
@@ -154,9 +154,10 @@ int c2fconnec_size, int fconnec_size, int nbParts, float cwipiVerbose, double ge
     //* Change the name of the coupling for each OF instance taking the rank of the proc *
 
     int myGlobalRank;
+    label nMyProc=Pstream::myProcNo();
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
     char couplingName[250] = {"cwipiFoamCoupling"};
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/nbParts);
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;
 
     char appSuffixChar[50];
     sprintf(appSuffixChar, "%i", appSuffix);
@@ -226,14 +227,15 @@ interpolationCellPointWallModified<vector> triangulateCellsU, float cwipiVerbose
     int myGlobalRank;
     char procChar[50];
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
-    int proc = (myGlobalRank - 1) % nbParts;
-    sprintf(procChar, "%i", proc);
+    label nMyProc=Pstream::myProcNo();
+    sprintf(procChar, "%i", nMyProc);
     // strcat(UIntGen, "/processor");
     // strcat(UIntGen, procChar);
     strcat(UIntGen, "/UInt");
 
-    std::string procString = std::to_string(proc);
-    int ens = floor((myGlobalRank + nbParts - 1)/nbParts); 
+    std::string procString = std::to_string(nMyProc);
+    int ens = floor((myGlobalRank - nMyProc)/nbParts) + 1;
+    
     std::string ensString = std::to_string(ens);
     std::string sampString = ensString + procString;
     char sampChar[500];
@@ -414,16 +416,19 @@ void cwipiSend(const fvMesh& mesh, const volVectorField& vf, const Time& runTime
     scalar t = runTime.value();
 
     int myGlobalRank;
+    label nMyProc=Pstream::myProcNo();
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;  // Normally works for any decomposition
+
     char couplingName[250] = {"cwipiFoamCoupling"};
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/nbParts); 
+
 
     char appSuffixChar[50];
     sprintf(appSuffixChar, "%i", appSuffix);
     strcat(couplingName, appSuffixChar);
 
     //* Send and wait for the receive *
-    if (cwipiVerbose) Pout<< "Before sending to KF in member "<< appSuffix << endl;  
+    if (cwipiVerbose) Pout<< "Before sending to KF in member "<< appSuffix << " from processor " << Foam::Pstream::myProcNo() << endl;  
     cwipi_issend(couplingName, "ex1", sendTag, 3, cwipiIteration, t, "u0,v0,w0", fieldsToSend, &status);
     cwipi_wait_issend(couplingName, status);
     //if (cwipiVerbose) Pout<< "After sending to KF in member "<< appSuffix << endl;
@@ -431,7 +436,7 @@ void cwipiSend(const fvMesh& mesh, const volVectorField& vf, const Time& runTime
     switch(status)
     {
         case CWIPI_EXCHANGE_OK :
-        if (cwipiVerbose) Pout << "Sent Ok in member " << appSuffix << endl << "\n";
+        if (cwipiVerbose) Pout << "Sent Ok in member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << endl << "\n";
         break;
         case CWIPI_EXCHANGE_BAD_RECEIVING :
         if (cwipiVerbose) Pout << "Bad receiving in member " << appSuffix << endl << "\n";
@@ -450,20 +455,62 @@ void cwipiSendParams(const fvMesh& mesh, const volVectorField& vf, const Time& r
     // double* ParamsToSend = new double[mesh.nCells()];
     double* ParamsToSend = new double[cwipiParams];
 
+    label nMyProc=Pstream::myProcNo();
     int myGlobalRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/ nbParts); // Change 2 by the number of subdomains
-    if (cwipiVerbose == 1) Pout << "Before sending params to KF from member " << appSuffix << endl; 
-    label top = mesh.boundaryMesh().findPatchID("movingWall");
-    double movingWallU = vf.boundaryField()[top][0].component(0);
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;  // Normally works for any decomposition
     
-    ParamsToSend[0] = movingWallU;
+    if (cwipiVerbose == 1) Pout << "Before sending params to KF from member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << endl; 
 
-    //* We use a basic MPI primitive because we don't want any interpolation performed by
-    //the cwipi primitive, the destination rank is always 0 because KF_coupling is supposed
-    // to always be 0 when we launch a calculation (first in the command line)*
-    MPI_Send(ParamsToSend, cwipiParams, MPI_DOUBLE, 0, sendTag_params, MPI_COMM_WORLD);
-    if (cwipiVerbose == 1) Pout << "After sending params to KF from member " << appSuffix << endl;
+    label top = mesh.boundaryMesh().findPatchID("movingWall");
+
+    int cellsCount = 0;
+    forAll(vf.boundaryField()[top],cells)
+    {
+        cellsCount = cellsCount + 1;
+    }
+    Pout << "What is inside movingWall boundary " << cellsCount << endl;
+
+    if(cellsCount > 0)
+    {
+        double movingWallU = vf.boundaryField()[top][0].component(0);
+        ParamsToSend[0] = movingWallU;
+    }
+
+    if (cwipiVerbose == 1) Pout << "After affection if find ok " << endl;
+
+    label n=Pstream::nProcs();
+    int tempFindMovingWall = 0;
+    double tempParam = 0;
+    if (Pstream::master())
+    {
+        for(int i=1; i<n; i++)
+        {
+            IPstream streamFromSlaves(Pstream::commsTypes::blocking, i);
+            streamFromSlaves >> tempFindMovingWall;
+            streamFromSlaves >> tempParam;
+            if(tempFindMovingWall > 0)
+            {
+                ParamsToSend[0] = tempParam;
+            }
+        }
+
+        //* We use a basic MPI primitive because we don't want any interpolation performed by
+        //the cwipi primitive, the destination rank is always 0 because KF_coupling is supposed
+        // to always be 0 when we launch a calculation (first in the command line)*
+        MPI_Send(ParamsToSend, cwipiParams, MPI_DOUBLE, 0, sendTag_params, MPI_COMM_WORLD);
+    }
+    else
+    {
+        // Create the input stream from processor i
+        OPstream stream2Main(Pstream::commsTypes::blocking, 0);
+        stream2Main << cellsCount;
+        stream2Main << ParamsToSend[0];
+    }
+
+
+    if (cwipiVerbose == 1) Pout << "After sending params to KF from member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << endl;
+    
 
     delete[] ParamsToSend;
 }
@@ -480,11 +527,14 @@ void cwipiRecv(const fvMesh& mesh, volVectorField& U, const Time& runTime, int c
     scalar t = runTime.value();
 
     int myGlobalRank;
+    label nMyProc=Pstream::myProcNo();
+
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;  // Normally works for any decomposition
+    
     char rankChar[50];
     sprintf(rankChar, "%i", myGlobalRank);
     char couplingName[250] = {"cwipiFoamCoupling"};
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/ nbParts);
 
     char appSuffixChar[50];
     sprintf(appSuffixChar, "%i", appSuffix);
@@ -500,7 +550,7 @@ void cwipiRecv(const fvMesh& mesh, volVectorField& U, const Time& runTime, int c
     switch(status2)
     {
         case CWIPI_EXCHANGE_OK :
-        if (cwipiVerbose) Pout << "Back-receive Ok in member " << appSuffix << endl;
+        if (cwipiVerbose) Pout << "Back-receive Ok in member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << endl;
         break;
         case CWIPI_EXCHANGE_BAD_RECEIVING :
         if (cwipiVerbose) Pout << "Bad Back-receiving in member " << appSuffix << "\n";
@@ -528,30 +578,69 @@ void cwipiRecvParams(const fvMesh& mesh, volVectorField& U, int cwipiParams, int
     //=== Receive equivalent of the boundary velocity send function ===
     
     double* paramsToRecv = new double[cwipiParams];
-
+    
+    label nMyProc=Pstream::myProcNo();
     int myGlobalRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/ nbParts); // Change 2 by the number of subdomains
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;  // Normally works for any decomposition
     
-    if (cwipiVerbose) Pout << "Before receive back params in member " << appSuffix << Foam::endl;
-
-    MPI_Recv(paramsToRecv, cwipiParams, MPI_DOUBLE, 0, recvTag_params, MPI_COMM_WORLD, &status4);
-
     label top = mesh.boundaryMesh().findPatchID("movingWall");
-    fvPatchVectorField& movingWallU = U.boundaryFieldRef()[top];
 
-    if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
-    
-    //* If all the velocities are the same througout the entire boundary, the U file in the timeStep resulting
-    // folders indicate a uniform boundary condition automatically *
-    forAll(movingWallU,faceI)
+    int cellsCount = 0;
+    forAll(U.boundaryField()[top],cells)
     {
-        movingWallU[faceI]=vector(paramsToRecv[0],0,0);
+        cellsCount = cellsCount + 1;
     }
+    Pout << "What is inside movingWall boundary " << cellsCount << endl;
 
-    if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
-    
-    if (cwipiVerbose) Pout << "After receive back params in member " << appSuffix << Foam::endl << "\n";
+    label n=Pstream::nProcs();
+    if (Pstream::master()){
+        if (cwipiVerbose) Pout << "Before receive back params in member " << appSuffix  << " from processor " << Foam::Pstream::myProcNo() << Foam::endl;
+        MPI_Recv(paramsToRecv, cwipiParams, MPI_DOUBLE, 0, recvTag_params, MPI_COMM_WORLD, &status4);
+
+        for(int i=1; i<n; i++)
+        {
+            // Create the input stream from processor i
+            OPstream stream2Slaves(Pstream::commsTypes::blocking, i);
+            stream2Slaves << paramsToRecv[0];
+        }
+        
+        if(cellsCount > 0)
+        {
+            fvPatchVectorField& movingWallU = U.boundaryFieldRef()[top];
+            if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
+            //* If all the velocities are the same througout the entire boundary, the U file in the timeStep resulting
+            // folders indicate a uniform boundary condition automatically *
+            forAll(movingWallU,faceI)
+            {
+                movingWallU[faceI]=vector(paramsToRecv[0],0,0);
+            }
+            if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
+        }
+
+        if (cwipiVerbose) Pout << "After receive back params in member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << Foam::endl << "\n";
+    }
+    else{
+        if (cwipiVerbose) Pout << "Before receive back params in member " << appSuffix  << " from processor " << Foam::Pstream::myProcNo() << Foam::endl;
+        // create the stream to send to the main proc
+        IPstream streamFromMain(Pstream::commsTypes::blocking, 0);
+        streamFromMain >> paramsToRecv[0];
+
+        if(cellsCount > 0)
+        {
+            fvPatchVectorField& movingWallU = U.boundaryFieldRef()[top];
+            if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
+            //* If all the velocities are the same througout the entire boundary, the U file in the timeStep resulting
+            // folders indicate a uniform boundary condition automatically *
+            forAll(movingWallU,faceI)
+            {
+                movingWallU[faceI]=vector(paramsToRecv[0],0,0);
+            }
+            if (cwipiVerbose) Pout << movingWallU[0].component(0) << endl;
+        }
+
+        if (cwipiVerbose) Pout << "After receive back params in member " << appSuffix << " from processor " << Foam::Pstream::myProcNo() << Foam::endl << "\n";
+    }
 
     delete[] paramsToRecv;
 }
@@ -563,9 +652,11 @@ void cwipideleteCoupling(double* pointCoords, int* face_index, int* face_connect
     if (cwipiVerbose) Info << "Delete Cwipi coupling from OF" << endl << "\n";
 
     int myGlobalRank;
+    label nMyProc=Pstream::myProcNo();
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
     char couplingName[250] = {"cwipiFoamCoupling"};
-    int appSuffix = floor((myGlobalRank + nbParts - 1)/nbParts);
+    int appSuffix = floor((myGlobalRank - nMyProc)/nbParts) + 1;
+    
 
     char appSuffixChar[50];
     sprintf(appSuffixChar, "%i", appSuffix);
