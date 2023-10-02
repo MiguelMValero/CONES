7520ae11b9964e78c47f54f1133b2be7bcf56970
Lucas <lucas.villanueva@ensma.fr>
2023-10-02 16:35:52 +0200

    Pimple update + correctifs

---
+++ lib/cwipi0.12.0/include/cwipi_config.h
@@ -0,0 +1,35 @@
+#ifndef __CWP_CONFIG_H__
+#define __CWP_CONFIG_H__
+
+/*------------------------------------------------------------------------------
+ * Version (CWIPI_* variable are deprecated)
+ *-----------------------------------------------------------------------------*/
+
+#define CWP_VERSION       "0.12.0"
+#define CWIPI_VERSION       "0.12.0"
+
+#define CWP_VERSION_MAJOR 0
+#define CWP_VERSION_MINOR 12
+#define CWP_VERSION_PATCH 0
+
+#define CWP_MAJOR_VERSION 0
+#define CWP_MINOR_VERSION 12
+#define CWP_RELEASE_VERSION 0
+
+/* #undef HAVE_MPI */
+
+/* #undef HAVE_MPI_IO */
+
+/* #undef HAVE_MPI_ONE_SIDED */
+
+/* #undef HAVE_SPACE_BASIS */
+
+/* #undef CWP_HAVE_BLAS */
+
+/* #undef CWP_HAVE_LAPACK */
+
+/* #undef CWP_HAVE_MKL */
+
+/* #undef CWP_HAVE_NOT_FORTRAN_IN_C */
+
+#endif /* __CWP_CONFIG_H__ */
