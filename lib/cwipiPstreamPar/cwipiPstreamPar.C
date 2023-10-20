3e81732760eea5a696072f6830b6efbcd6e07de4
MiguelMValero <miguel.martinez_valero@ensam.eu>
2023-10-20 16:47:08 +0200

    Add pressure option in pitzDaily case

--- lib/cwipiPstreamPar/cwipiPstreamPar.C
+++ lib/cwipiPstreamPar/cwipiPstreamPar.C
@@ -242,9 +242,9 @@ interpolationCellPoint<vector> triangulateCellsU, float cwipiVerbose, std::strin
     int ens = round((myGlobalRank - nMyProc - 1)/nbParts) + 1;
     
     std::string ensString = std::to_string(ens);
-    std::string sampString = ensString + procString;
-    char sampChar[500];
-    strcpy(sampChar, sampString.c_str());
+    // std::string sampString = ensString + procString;
+    // char sampChar[500];
+    // strcpy(sampChar, sampString.c_str());
     char ensChar[500];
     strcpy(ensChar, ensString.c_str());
 
@@ -400,6 +400,156 @@ interpolationCellPoint<vector> triangulateCellsU, float cwipiVerbose, std::strin
     //     myfile_2.close();
     // }
 
+    if (cwipiVerbose) if (Pstream::master()) Pout << "Sampling files created " << endl;
+}
+
+void pInterpolation(volScalarField& p, fvMesh& mesh, Time& runTime, int cwipiObsp, int nbParts,
+interpolationCellPoint<double> triangulateCellsp, float cwipiVerbose, std::string globalPath, std::string pIntPath)
+{
+    //========== Produce a file for each OF instance containing the sampled pressures 
+    //(H.x term in the kalman gain calculation) ========== 
+    
+    //========== Path of the correct OF instance ==========
+    std::string obs_file_path = globalPath + "/obs_coordinates.txt";
+    if (cwipiVerbose) if (Pstream::master()) Pout << "The path where I have my observation coordinates is " << obs_file_path << endl;
+    char pIntGen[500];
+    strcpy(pIntGen, pIntPath.c_str());
+
+    int myGlobalRank;
+    char procChar[50];
+    MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
+
+    label nMyProc=Pstream::myProcNo();
+    std::string procString = std::to_string(nMyProc);
+    Info << "nMyProc = " << nMyProc << endl; 
+
+    sprintf(procChar, "%i", nMyProc);
+    // strcat(pIntGen, "/processor");
+    // strcat(pIntGen, procChar);
+    strcat(pIntGen, "/pInt");
+
+    label n=Pstream::nProcs();
+    int ens = round((myGlobalRank - nMyProc - 1)/nbParts) + 1;
+    
+    std::string ensString = std::to_string(ens);
+    // std::string sampString = ensString + procString;
+    // char sampChar[500];
+    // strcpy(sampChar, sampString.c_str());
+    char ensChar[500];
+    strcpy(ensChar, ensString.c_str());
+
+    // char* pIntPart_path = strcat(pIntGen, sampChar);
+    char* pIntEns_path = strcat(pIntGen, ensChar);
+
+    if (cwipiVerbose) if (Pstream::master()) Foam::Pout<< "The file where pInt is created is " << pIntEns_path << endl;
+
+    //* Configuration of streams to open the files : observation coordinates, sampling partioned file, member sampling file *
+    std::ifstream obs_file;
+    obs_file.open(obs_file_path);
+
+    // std::ofstream UintPart_file;
+    // UintPart_file.open(UIntPart_path, std::ios::out | std::ios::app);
+
+    // string data = "";
+    // int columns = 0;
+    // int parameters = 1;
+
+     if (cwipiVerbose) if (Pstream::master()) Foam::Pout<< "Creating the sampling files..." << endl;
+
+    point pointCoord;
+    int cellIndex[cwipiObsp] = {0};
+    double pp[cwipiObsp] = {0};
+    
+    if (obs_file.is_open())
+    {  
+        std::string line;
+        int countProbes = 0;
+        while( std::getline(obs_file,line) )
+        {
+            std::stringstream ss(line);
+
+            std::string coordX, coordY, coordZ;
+            std::getline(ss, coordX,',');
+            pointCoord.x() = stod(coordX);
+            std::getline(ss, coordY,',');
+            pointCoord.y() = stod(coordY);
+            std::getline(ss, coordZ,','); 
+            pointCoord.z() = stod(coordZ);
+
+            label ownCell = mesh.findCell(pointCoord);
+            if (ownCell != -1){
+                pp[countProbes] = triangulateCellsp.interpolate(pointCoord, ownCell, -1);
+                cellIndex[countProbes] = countProbes+1; //index in the list of observations, we do not use 0 because it corresponds to no cell in the domain
+            }
+            ++countProbes;
+        }
+    }
+    else{
+        Foam::Pout << " Not able to open the obs_coordinates.txt file " << endl;
+        // printing the error message
+        fprintf(stderr, "File does not exist\n");
+    }
+    obs_file.close();
+                
+    /* Gather all the values from the procs and then write the new values in a txt file */
+    if (Pstream::master()){
+        /* Gather the values */
+        int tempCellIndex = 0;
+        double tempp = 0; 
+
+        for(int i=1; i<n; i++)
+        {
+            // Pout << "Receiving sampled data from proc " << i << endl;  
+                // create the input stream from processor i
+            IPstream streamFromMain(Pstream::commsTypes::blocking, i);
+            for (int i = 0; i < cwipiObsp; ++i){
+                streamFromMain >> tempCellIndex;
+                streamFromMain >> tempp;
+                cellIndex[i] = cellIndex[i] + tempCellIndex;
+                p[i] = p[i] + tempp;
+            }
+        }
+
+        /* Write them */
+        remove(pIntEns_path);
+        std::ofstream pintEns_file;
+        pintEns_file.open(pIntEns_path, std::ios::out | std::ios::app);
+        if (cwipiVerbose) Foam::Pout << "I am about to write the sampling file" << endl;
+        if (pintEns_file.is_open()){
+            // for (int i = 0; i < cwipiObsU; ++i){
+            //     UintEns_file << cellIndex[i] << "\n";
+            // }
+            for (int i = 0; i < cwipiObsp; ++i){
+                
+                pintEns_file << p[i] << "\n";
+            }
+        }
+        else{
+            std::cerr << "Not able to open the sampling file" << std::endl;
+        }
+        pintEns_file.close();
+    }
+    else{
+        // create the stream to send to the main proc
+        OPstream stream2Main(Pstream::commsTypes::blocking, 0);
+        for (int i = 0; i < cwipiObsp; ++i){
+            stream2Main << cellIndex[i];
+            stream2Main << p[i];
+        }
+    }
+
+        //========== Retrieve the cellIDs of my probes ==========//
+    // if (myGlobalRank == 1){
+    //     std::ofstream myfile_2;
+    //     std::string cellSensors = pIntPath + "/results/cellIDs";
+
+    //     if (remove(cellSensors.c_str()) != 0)
+    //         perror( "Error deleting file with cell IDs of the sensors" );
+    //     myfile_2.open(cellSensors, std::ios::out | std::ios::app);
+    //         for (int i = 0; i < cwipiObsp; ++i){
+    //             myfile_2 << cellID[i] << "\n";
+    //         }
+    // }
     if (cwipiVerbose) if (Pstream::master()) Pout << "Sampling files created " << endl;
 }
 
@@ -794,12 +944,14 @@ void cwipiRecvParamsKEps(const fvMesh& mesh, incompressible::momentumTransportMo
     double* paramsToRecv = new double[cwipiParams];
 
     label nMyProc=Pstream::myProcNo();
+    if (cwipiVerbose) if (Pstream::master()) Pout << "nMyProc is: " << nMyProc << endl;
     int myGlobalRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myGlobalRank);
+    if (cwipiVerbose) if (Pstream::master()) Pout << "myGlobalRank is: " << myGlobalRank << endl;
     int appSuffix = round((myGlobalRank - nMyProc - 1)/nbParts) + 1; // Normally works for any decomposition
 
     if (Pstream::master()){
-        std::string dictPath = globalRootPath+"/member"+std::to_string(myGlobalRank-1)+"/constant/momentumTransport";
+        std::string dictPath = globalRootPath+"/member"+std::to_string(appSuffix)+"/constant/momentumTransport";
 
         if (cwipiVerbose) Pout<< "dictPath = " << dictPath << endl; 
 
